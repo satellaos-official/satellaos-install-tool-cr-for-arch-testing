81b989b7b817a963465544efcb527c46df991c92
satella <forgottensatella@gmail.com>
2026-06-25 15:46:17 +0300

    Trying Fixing yay script

--- tree-installer-system/yay/run.sh
+++ tree-installer-system/yay/run.sh
@@ -2,10 +2,16 @@
 
 echo "Installing the yay"
 
-sudo pacman -S --noconfirm --needed git base-devel
+sudo pacman -S --needed git base-devel
 
-git clone https://aur.archlinux.org/yay.git /tmp/yay
+git clone https://aur.archlinux.org/yay.git
 
-makepkg -si --noconfirm -C /tmp/yay
+cd yay
+
+makepkg -si
+
+cd $HOME/satellaos-install-tool-cr-for-arch-testing/
+
+rm -rf $HOME/satellaos-install-tool-cr-for-arch-testing/yay/
 
 echo "yay Installing is Complete"
