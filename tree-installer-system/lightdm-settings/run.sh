235d76c14224eab62e073e835bf7946987b54ebe
satella <forgottensatella@gmail.com>
2026-06-25 18:02:15 +0300

    nineth Commit

--- tree-installer-system/lightdm-settings/run.sh
+++ tree-installer-system/lightdm-settings/run.sh
@@ -4,5 +4,7 @@ echo "Updating The LightDM Settings"
 
 Base="$HOME/satellaos-install-tool-cr-for-arch-testing/tree-installer-system/lightdm-settings/lightdm"
 
+sudo mkdir -p /etc/lightdm
+
 sudo cp "$Base/lightdm.conf" /etc/lightdm/lightdm.conf
 sudo cp "$Base/lightdm-gtk-greeter.conf" /etc/lightdm/lightdm-gtk-greeter.conf
